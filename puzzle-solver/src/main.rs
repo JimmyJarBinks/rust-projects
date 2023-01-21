b129b1527bd8c0effecd702c51942ef73b3c2f9d
KennethHotra <kenny.hotra@gmail.com>
2023-01-21 10:57:45 -0700

    Implement Tests
    Will want to move tests to separate file and adopt TDD for remainder of project.

--- puzzle-solver/src/main.rs
+++ puzzle-solver/src/main.rs
@@ -2,16 +2,16 @@ use std::{env, error::Error, fs, process};
 
 const SUDOKU_SIZE: u32 = 9;
 
-enum PuzzleType {
-    Sudoku,
-}
-
 trait Puzzle {
     fn build(contents: &mut String) -> Result<Self, Box<dyn Error>>
     where
         Self: Sized;
     fn is_valid(&self) -> bool;
     fn solve_puzzle(&self);
+}
+
+enum PuzzleType {
+    Sudoku,
 }
 
 struct Sudoku {
@@ -31,7 +31,7 @@ impl Puzzle for Sudoku {
             })
         } else {
             Err(Box::from(
-                "Failed to read puzzle. For sudoku, ensure there are 81 digits total and no non-digit characters .",
+                "Failed to read puzzle. For sudoku, ensure there are 81 digits total and no non-digit characters (whitespace is acceptable).",
             ))
         }
     }
@@ -43,6 +43,7 @@ impl Puzzle for Sudoku {
     fn solve_puzzle(&self) {}
 }
 
+#[derive(Debug, PartialEq)]
 struct Command {
     puzzle: String,
     filename: String,
@@ -54,7 +55,7 @@ impl Command {
 
         let puzzle = match args.next() {
             Some(arg) => arg.to_lowercase(),
-            None => return Err("At least 1 argument required to specify puzzle type."),
+            None => return Err("At least 1 argument is required to specify puzzle type."),
         };
 
         let filename = match args.next() {
@@ -77,7 +78,7 @@ fn sudoku_puzzle(contents: &mut String) -> Result<(), Box<dyn Error>> {
 fn run(command: Command) -> Result<(), Box<dyn Error>> {
     let puzzle: PuzzleType = match command.puzzle.as_str() {
         "sudoku" => PuzzleType::Sudoku,
-        _ => return Err(Box::from("Specified puzzle not supported")),
+        _ => return Err(Box::from("The specified puzzle is not supported.")),
     };
 
     let mut contents = fs::read_to_string(command.filename)?;
@@ -94,17 +95,58 @@ fn run(command: Command) -> Result<(), Box<dyn Error>> {
 fn main() {
     let command = Command::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Application Error: {err}");
-        process::exit(1);
+        process::exit(0);
     });
     if let Err(err) = run(command) {
         eprintln!("Application Error: {err}");
-        process::exit(1);
+        process::exit(0);
     }
 }
 
-/*
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    #[test]
+    fn test_empty_command() {
+        let args = [String::new(); 0];
+        let command = Command::build(args.into_iter());
+        assert_eq!(
+            command,
+            Err("At least 1 argument is required to specify puzzle type.")
+        )
+    }
+    // TODO: Compare errors
+    #[test]
+    fn test_unsupported_puzzle() {
+        let result = run(Command {
+            puzzle: "foobar".to_string(),
+            filename: "foobar.txt".to_string(),
+        })
+        .unwrap_err();
+        assert_eq!(result.to_string(), "The specified puzzle is not supported.")
+    }
+    // TODO: Compare errors
+    #[test]
+    fn test_file_not_found() {
+        let _result = run(Command {
+            puzzle: "sudoku".to_string(),
+            filename: "".to_string(),
+        })
+        .unwrap_err();
+        assert!(false)
+        //assert_eq!(result, io::ErrorKind::NotFound)
+    }
+    // TODO: Compare errors
+    #[test]
+    fn test_bad_sudoku_file() {
+        let mut contents = String::from("12345678901234546002546486206p2345904-234589 45869");
+        let _sudoku = Sudoku::build(&mut contents);
+        assert!(false)
+    }
+    // TODO: Test Unsolvable Puzzle
+    #[test]
+    fn test_unsolvable_sudoku() {
+        assert!(false)
+    }
 }
-*/
