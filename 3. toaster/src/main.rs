24dc465ec34912facf2b0d4b74b0f5c9f92846cf
KennethHotra <kenny.hotra@gmail.com>
2023-01-28 11:36:57 -0700

    Implement Time Interval

--- 3. toaster/src/main.rs
+++ 3. toaster/src/main.rs
@@ -1,19 +1,38 @@
-use winrt_notification::{Duration, Sound, Toast};
+use std::time::Duration;
+use chrono::Local;
+use winrt_notification::{Sound, Toast};
 
-fn create_notification(title: &str, text1: &str, text2: &str) -> Toast {
+const INTERVAL_TIME: u64 = 10;
+
+/*
+fn configure_message() -> (String, String, String) {
+    (String::new(), String::new(), String::new())
+}
+*/
+
+async fn create_notification(title: &str, text1: &str, text2: &str) -> Toast {
     Toast::new(Toast::POWERSHELL_APP_ID)
         .title(title)
         .text1(text1)
         .text2(text2)
         .sound(Some(Sound::SMS))
-        .duration(Duration::Short)
+        .duration(winrt_notification::Duration::Short)
 }
 
-fn main() {
-    let toast = create_notification(
-        "Remember to Sit Up Straight",
-        "Or better yet, take a quick break",
-        "Your back will thank you later.",
-    );
-    toast.show().expect("unable to toast");
+#[tokio::main]
+async fn main() {
+    let mut interval = tokio::time::interval(Duration::from_secs(INTERVAL_TIME));
+    loop {
+        interval.tick().await;
+        let toast = create_notification(
+            "Remember to sit up straight",
+            "Or better yet, take a quick break.",
+            "Your back will thank you later.",
+        );
+
+        let dt = Local::now();
+        println!("{}", dt);
+
+        toast.await.show().expect("unable to toast");
+    }
 }
